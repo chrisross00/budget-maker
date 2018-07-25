8d31b0e68595358fffd9eb42bec18074b6383fa0
chris_macbook <chrisross00@gmail.com>
2018-07-25 07:35:45 -0400

    Init commit

---
+++ src/components/LoginPage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { startLogin } from '../actions/auth';
+
+export const LoginPage = ({ startLogin }) => (
+  <div className="box-layout">
+    <div className="box-layout__box">
+      <h1 className="box-layout__title">Budget Maker</h1>
+      <p>Make a budget. Set goals. Find freedom.</p>
+      <button
+        className="button"
+        onClick={startLogin}>Login with Google</button>
+    </div>
+  </div>
+);
+
+const mapDispatchToProps = (dispatch) => ({
+  startLogin: () => dispatch(startLogin())
+});
+
+export default connect(undefined, mapDispatchToProps)(LoginPage);
